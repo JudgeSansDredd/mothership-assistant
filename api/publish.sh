3cbd7a26cda4469a548c81178f945d8e79a74d13
Nathan Stanley <nathan@pixelparasol.com>
2025-07-17 19:32:37 -0400

    Add publish scripts

---
+++ api/publish.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+npm run build
+
+DOCKER_REGISTRY="gitea.pixelparasol.com/nathan/game-library" && CI_COMMIT_SHORT_SHA=$(git rev-parse --short HEAD)
+
+docker buildx build --no-cache -f Dockerfile . --platform linux/amd64 -t $DOCKER_REGISTRY/api:latest -t $DOCKER_REGISTRY/api:$CI_COMMIT_SHORT_SHA
+
+docker login gitea.pixelparasol.com
+
+docker push $DOCKER_REGISTRY/api:$CI_COMMIT_SHORT_SHA
+
+docker push $DOCKER_REGISTRY/api:latest
